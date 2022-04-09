ae38656b810d2a35577f206fdc799db49c2b5d9f
thomaslonowski <thomaslonowski@onyxnode47.boisestate.edu>
2022-04-08 18:19:16 -0600

    IUDoubleLinkedList
    Commented out mention of SLL.

--- List Tester/src/ListTester.java
+++ List Tester/src/ListTester.java
@@ -297,9 +297,9 @@ public class ListTester {
 //			case arrayList:
 //				listToUse = new IUArrayList<Integer>();
 //				break;
-			case singleLinkedList:
-				listToUse = new IUSingleLinkedList<Integer>();
-				break;
+//			case singleLinkedList:
+//				listToUse = new IUSingleLinkedList<Integer>();
+//				break;
 			case doubleLinkedList:
 				listToUse = new IUDoubleLinkedList<Integer>();
 				break;
