38a7a36da4a719efdb7506916386b16942c6ed05
Thomas Lonowski <tomo.lonowski@gmail.com>
2022-04-19 16:03:14 -0600

    In-class work Tuesday

--- Random Code/src/Recursion.java
+++ Random Code/src/Recursion.java
@@ -23,5 +23,73 @@ public class Recursion {
 		return reverseStr;
 	}
 	
+	private static <T extends Comparable<T>> void quickSort(IndexedUnsortedList<T> list) {
+		// Base case
+		if(list.size() <= 1) {
+			return;
+		}
+		
+		// Grab pivot
+		T pivot = list.removeFirst();
+		
+		//Partition remaining elements
+		IndexedUnsortedList<T> left = newList();
+		IndexedUnsortedList<T> right = newList();
+		while(!list.isEmpty()) {
+			if(list.first().compareTo(pivot) < 0) {	// first < pivot
+				left.add(list.first());
+			} else {
+				right.add(list.first());
+			}
+		}
+		
+		// Recursively sort left and right lists
+		quickSort(left);
+		quickSort(right);
+		
+		// Reassemble the final list
+		while(!left.isEmpty()) {
+			list.add(left.removeFirst());
+		}
+		list.add(pivot);
+		while(!right.isEmpty()) {
+			list.add(right.removeFirst());
+		}		
+	}
+	
+	private static <T> void quickSort(IndexedUnsortedList<T> list, Comparator<T> c) {
+		// Base case
+		if(list.size() <= 1) {
+			return;
+		}
+		
+		// Grab pivot
+		T pivot = list.removeFirst();
+		
+		//Partition remaining elements
+		IndexedUnsortedList<T> left = newList();
+		IndexedUnsortedList<T> right = newList();
+		while(!list.isEmpty()) {
+			if(c.compare(list.first(), pivot) < 0) {	// diff
+				left.add(list.first());
+			} else {
+				right.add(list.first());
+			}
+		}
+		
+		// Recursively sort left and right lists
+		quickSort(left, c);		// diff
+		quickSort(right, c);	// diff
+		
+		// Reassemble the final list
+		while(!left.isEmpty()) {
+			list.add(left.removeFirst());
+		}
+		list.add(pivot);
+		while(!right.isEmpty()) {
+			list.add(right.removeFirst());
+		}		
+	}
+
 	
 }
